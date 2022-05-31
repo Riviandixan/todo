47cb2b3436e78cd1acfa6e48ba3181f3c31f23f6
Riviandixan <riviandixanpurnadi@smkwikrama.sch.id>
2022-05-31 20:57:36 +0700

    todo

---
+++ assets/main.js
@@ -0,0 +1,51 @@
+const addForm = document.querySelector("form.add");
+const ul = document.querySelector("ul.todos");
+const searchFormInput = document.querySelector("form.search input");
+
+//ADD NEW TODO
+const handleAddItem = (inputValue) => {
+  const html = `
+   <li class="list-group-item d-flex justify-content-between align-items-center">
+      <span>${inputValue}</span>
+      <i class="far fa-trash-alt delete">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+        <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+        </svg>
+      </i>
+    </li>
+  `;
+  ul.innerHTML += html;
+};
+
+addForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const inputValue = addForm.add.value;
+  if (inputValue.length) handleAddItem(inputValue);
+  addForm.add.value = "";
+});
+
+//REMOVE TODO
+ul.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete")) {
+    e.target.parentElement.remove();
+  }
+});
+
+//SEARCH INPUT: PREVENT DEFAULT ACTION - LITTLE BUG I FOUND IN THE COURSE PROJECT
+searchFormInput.parentElement.addEventListener("submit", (e) =>
+  e.preventDefault()
+);
+
+//SEARCH AND FILTER TODOS
+const filterItems = (value) => {
+  Array.from(ul.children).forEach((li) => {
+    if (!li.textContent.toLowerCase().includes(value)) li.classList.add("filtred");
+    else li.classList.remove("filtred");
+  });
+};
+
+searchFormInput.addEventListener("keyup", (e) => {
+  const value = searchFormInput.value.toLowerCase().trim();
+  filterItems(value);
+});
